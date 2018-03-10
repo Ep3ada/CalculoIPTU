126cbad22032ed84602747b4839c06b148937206
Ricardo Santos <ep3ada@gmail.com>
2018-03-10 00:13:33 -0300

    POC JUnit do calculo de iptu; 6 testes

---
+++ calculoIptu/src/main/java/br/com/bandtec/calculoiptu/CalcularIptu.java
@@ -0,0 +1,51 @@
+package br.com.bandtec.calculoiptu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalcularIptu {
+
+    public boolean isCidadeValida(String c) {
+        if (c.equals("São Paulo")) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private double calculoValorIptu(String cidade, double valor) {
+        if (!isCidadeValida(cidade)) {
+            throw new IllegalArgumentException("Cidade inexistente na base de dados");
+        }
+        if (valor < 0) {
+            throw new IllegalArgumentException("Digite valor de imóvel válido");
+        }
+
+        double resultado = 0;
+
+        if (valor <= 150000) {
+            resultado = 0;
+        } else if (valor > 150000 && valor <= 300000) {
+            resultado = valor * 0.009;
+        } else if (valor > 300000 && valor <= 600000) {
+            resultado = valor * 0.011;
+        } else {
+            resultado = valor * 0.013;
+        }
+
+        return resultado;
+    }
+
+    public double calcularPagaAVista(String cidade, double valor) {
+        double resultado = calculoValorIptu(cidade, valor);
+
+        return resultado - (resultado * 0.15);
+    }
+
+    public double calcularValor1Parcela(String cidade, double valor) {
+        double resultado = calculoValorIptu(cidade, valor);
+        return resultado * 0.1;
+    }
+
+}
---
+++ calculoIptu/src/test/java/unit/br/com/bandtec/CalculoIptuTest.java
@@ -0,0 +1,135 @@
+package unit.br.com.bandtec;
+
+import static org.junit.Assert.*;
+
+import br.com.bandtec.calculoiptu.CalcularIptu;
+import org.junit.Test;
+
+public class CalculoIptuTest {
+
+    @Test
+    public void testeFaixa3() {
+        CalcularIptu iptu = new CalcularIptu();
+
+        double valor = 300000.0;
+        double resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        double esperado = (valor * 0.011);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+
+        valor = 300001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.011);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 500000.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.011);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 600000.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.011);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 600001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.011);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+    }
+
+    @Test
+    public void testeFaixa2() {
+        CalcularIptu iptu = new CalcularIptu();
+
+        double valor = 150000.0;
+        double resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        double esperado = (valor * 0.009);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+
+        valor = 150001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.009);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 200000;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.009);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 300000.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.009);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 300001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.009);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+    }
+
+    @Test
+    public void testeFaixa4() {
+        CalcularIptu iptu = new CalcularIptu();
+
+        double valor = 600000.0;
+        double resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        double esperado = (valor * 0.013);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+
+        valor = 600001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.013);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+    }
+
+    @Test
+    public void testeFaixa1() {
+        CalcularIptu iptu = new CalcularIptu();
+
+        double valor = 60000.0;
+        double resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        double esperado = (valor * 0.0);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 150000.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.0);
+        esperado = esperado - (esperado * 0.15);
+        assertEquals(esperado, resultado, 0);
+
+        valor = 150001.0;
+        resultado = iptu.calcularPagaAVista("São Paulo", valor);
+        esperado = (valor * 0.0);
+        esperado = esperado - (esperado * 0.15);
+        assertNotEquals(esperado, resultado, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testarErroEmNumeroNegativo() {
+        CalcularIptu iptu = new CalcularIptu();
+        double valor = -100000.0;
+        double resultado = iptu.calcularPagaAVista("São Paulo", valor);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testarErroEmNomeCidade() {
+        CalcularIptu iptu = new CalcularIptu();
+        double valor = 100000.0;
+        double resultado = iptu.calcularPagaAVista("São P", valor);
+
+    }
+}
