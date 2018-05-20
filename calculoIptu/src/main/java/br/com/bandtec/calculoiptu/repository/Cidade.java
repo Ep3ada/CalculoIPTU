fc5e702a23fd9a3b0371c833435ccdedbefe4bc7
Ricardo Santos <ep3ada@gmail.com>
2018-05-20 16:09:29 -0300

    Foram apagados .java errados em repository

--- calculoIptu/src/main/java/br/com/bandtec/calculoiptu/repository/Cidade.java
+++
@@ -1,8 +0,0 @@
-package br.com.bandtec.calculoiptu.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Cidade extends
-          CrudRepository<Cidade, Integer> {
-
-}
--- calculoIptu/src/main/java/br/com/bandtec/calculoiptu/repository/Contribuinte.java
+++
@@ -1,7 +0,0 @@
-package br.com.bandtec.calculoiptu.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Contribuinte extends CrudRepository<Contribuinte, Integer> {
-
-}
